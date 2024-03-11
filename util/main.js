d2d57f5c77c88089b123c3373ae6514058ab649d
Joseph <josephkim0703@gmail.com>
2024-03-11 16:07:52 -0700

    update

--- util/main.js
+++ util/main.js
@@ -1,12 +1,8 @@
 import { apps } from "./app.js";
 
-
-
-function taskview() {
-    let hover = null;
-
-    if(hover){
-        document.getElementById("task_view").src = "./assets/"
-        hover = !hover;
+class Window{
+    constructor(){
+    
     }
+    
 }
