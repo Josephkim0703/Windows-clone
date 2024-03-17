195ea6931b5deff9d091014494db2c03f8675497
Joseph <josephkim0703@gmail.com>
2024-03-16 18:47:52 -0700

    update

--- util/app.js
+++ util/app.js
@@ -1,5 +1,12 @@
 const apps = [
-
+    {
+        name: "chrome",
+        logo: "./assets/chrome.png",
+    },
+    {
+        name: "folder",
+        logo: "./assets/file.png"
+    }
 ]
 
 export {apps}
--- util/main.js
+++ util/main.js
@@ -1,8 +1,16 @@
 import { apps } from "./app.js";
 
-class Window{
-    constructor(){
-    
+/*
+function pinApp(apps){
+
+    let data = "";
+    let pinned = [];
+
+    for(let i = 0; i < apps.length; i++){
+        data += '<button type="button" id="app' + i + '>' + apps[i].logo + '</button>';
+        pinned.push(data);
     }
-    
+    document.getElementById("pin_taskbar").innerHTML = pinned;
 }
+
+pinApp(apps);*/
