cb99de97b06b41d17907c9a0c19ea47caea5996d
Joseph <josephkim0703@gmail.com>
2024-05-04 18:48:54 -0700

    bug fixes

--- util/main.js
+++ util/main.js
@@ -11,6 +11,9 @@ function main(){
     desktopApp(desktopapps);
 
     inject(desktopapps, grab);
+
+   
+  //  drag(desktopapps);
 }
 
 function pinApp(apps){
@@ -58,7 +61,6 @@ function time(){
 }
 
 //inject desktop active apps to taskbar and hidden tabs
-
 function inject( dapps, grab){
 
     for(let i = 0; i < dapps.length; i++){
@@ -76,30 +78,42 @@ function inject( dapps, grab){
         }
 
         if(clickcount === 2){
-
+          
             option.style.backgroundColor = "";
             option.style.border = "";
 
             clickcount = 0;
 
             //it grab this which is the current option then replaces the id app with nothing to isolate the number
-            let index = parseInt(this.id.replace("desktopApps", ""));   
+            let index = parseInt(this.id.replace("desktopApps", ""));
+            
+          
             
             //turns status on
             dapps[index].status = "on";
             //current tab open
             dapps[index].active = "yes";
-
+            
             grab(dapps[index]);
             
             //refreshes the taskbar and hidden tab
             pinApp(apps);
             hidden_icon(allapps); 
-            active(apps)  
+            active(apps) 
+            
+            openApp(apps, index); 
         }
     }
     option.addEventListener("click" , click)
     }
+}
+
+function taskbarclick(){
+
+}
+
+function hiddenclick(){
+    
 }
 
 function active(apps){
@@ -119,12 +133,100 @@ function active(apps){
         }
     }  
 }
+//problem the i for dapps is 0 but the array is already 3 so when it gets pushed in the i doesnt change
+function openApp(apps, index){
+
+    const ulElement = document.getElementById("desktop_box");
+            //creates a new box element
+            const box = document.createElement("div");
+            box.classList.add("box"); 
+            box.style.visibility = "visible";
+       
+            const closeButton = button("X", "app_close", index, box);
+            const sizeButton = button("=", "app_size", index, box);
+            const hideButton = button("-", "app_hidden", index, box);
+
+            [hideButton , sizeButton, closeButton].forEach(button => {box.appendChild(button)});
+
+            ulElement.appendChild(box);
+
+    function button(content, id, index, box){
+        const button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("id", id);
+        button.addEventListener("click", function() {
+            if (id === "app_close") {
+                closetab(index, box);
+            } else if (id === "app_size") {
+                sizetab(index, box);
+            } else if (id === "app_hidden") {
+                hidetab(index, box);
+            }
+            });
+        button.textContent = content;
+        return button;
+    }
+
+    function closetab(i, box){
+        apps[i].active = "no";
+        apps[i].status = "off";
+        box.style.visibility = "hidden";
+        //fix this pop method to pop the specific index not the last index in the array
+        apps.pop();
+        allapps.pop();
+        pinApp(apps);
+        hidden_icon(allapps); 
+        console.log(apps);
+    }
+
+    function sizetab(i){
+
+        let fullsize = false;
+
+        if(!fullsize){
+
+        }else{
+
+            fullsize = !fullsize;  
+        }
+   
+    }
+
+    function hidetab(i, box){   
+        apps[i].active = "no";
+        box.style.visibility = "hidden";
+        pinApp(apps);
+        hidden_icon(allapps); 
+        console.log(apps);
+    }
+}
 
 function drag(app){
 
-    document.addEventListener("drag", evt => {
+    document.addEventListener("DOMContentLoaded", function() {
+
+     
+        const application = document.getElementById("desktopApps0" );
+
+        let X, Y, drag = false;
+
+        application.addEventListener("dragstart", e => {
+            X = e.clientX - application.getBoundingClientRect().left;
+            Y = e.clientY - application.getBoundingClientRect().top;
+            drag = true;
+        });
+
+        document.addEventListener("mousemove", e => {
+            if(!drag) return;
+
+            const x = e.client - X;
+            const y = e.client - Y;
+
+            application.style.top = x + "px";
+            application.style.left = y + "px";
+        });
         
-    })
-}
+    });
+};
 
 main();
